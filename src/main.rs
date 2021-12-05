c5b8055c4e0a40904c80c31acb0f951f87b9cb60
Eugene Wang <eugenewyj79@gmail.com>
2021-12-05 22:58:49 +0800

    学习时的练习代码

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use structopt::StructOpt;
+
+#[derive(Debug, StructOpt)]
+struct Cli {
+    pattern: String,
+
+    #[structopt(parse(from_os_str))]
+    path: std::path::PathBuf,
+}
+
 fn main() {
-    println!("Hello, world!");
+    // let parttern = std::env::args().nth(1).expect("未指定要搜索的匹配模式");
+    // let path = std::env::args().nth(2).expect("未指定要搜到的文件路径");
+
+    // let cli = Cli {
+    //         pattern: parttern,
+    //         path: std::path::PathBuf::from(path),
+    //     };
+    // println!("要搜索的匹配模式是:{:?}", parttern);
+    // println!("要搜索的文件路径是:{:?}", path);
+
+    let cli = Cli::from_args();
+    println!("参数是：{:?}", cli);
 }
